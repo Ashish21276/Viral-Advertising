0598944a3b979007c12a631c434de3eeda4677b5
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2021-03-27 10:55:06 +0530

    Create Viral_Advertising.java
    Viral Advertising
    
    HackerRank Solution in Java

---
+++ Viral_Advertising.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int viralAdvertising(int n) {
+        int x=2;
+        int y=2;
+        for(int i=1;i<n;i++)
+        {
+            x=x*3;
+            x=x/2;
+            y=y+x;
+        }
+        return y;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int result = viralAdvertising(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
